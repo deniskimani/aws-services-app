28427e5d50d3eae4bdd9a814ed5bd667d53d7e8a
deniskimani <48308527+deniskimani@users.noreply.github.com>
2023-08-16 17:52:57 +0300

    crud added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+require('dotenv').config()
+const db = require('./queries')
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.status(200).json(`My API is running 😎 huh?!`);
 });
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 module.exports = app;
